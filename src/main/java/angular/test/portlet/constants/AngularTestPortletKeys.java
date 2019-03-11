5113305f5ba038dda2d83d79f5b26087068111c8
Evan Thibodeau <evan.thibodeau@liferay.com>
2019-03-11 17:09:32 +0300

    init

---
+++ src/main/java/angular/test/portlet/constants/AngularTestPortletKeys.java
@@ -0,0 +1,10 @@
+package angular.test.portlet.constants;
+
+/**
+ * @author evanthibodeau
+ */
+public class AngularTestPortletKeys {
+
+	public static final String AngularTest = "angulartest";
+
+}
